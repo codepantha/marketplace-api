ec5132b08b6d57be58894620acb7473d018492a6
codepanther <pezechigozirim@gmail.com>
2023-04-04 21:48:39 +0100

    Write tests for, and add the user create endpoint

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,16 +1,36 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
+require 'test_helper'
 
-  test "should show user" do
-    get api_v1_user_url(@user), as: :json
-    assert_response :success
+module Api
+  module V1
+    class UsersControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        @user = users(:one)
+      end
 
-    # test to ensure response contains the correct email
-    json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
+      test 'should show user' do
+        get api_v1_user_url(@user), as: :json
+        assert_response :success
+
+        # test to ensure response contains the correct email
+        json_response = JSON.parse(response.body)
+        assert_equal @user.email, json_response['email']
+      end
+
+      test 'should create user' do
+        assert_difference('User.count') do
+          post api_v1_users_url, params: { user: { email: 'test@test.com', password: '123456' } }, as: :json
+        end
+        assert_response :created
+      end
+
+      test 'should not create user when email is taken' do
+        assert_no_difference('User.count') do
+          post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
+        end
+        assert_response :unprocessable_entity
+      end
+    end
   end
 end
